a55f91ea0d126eb2a372f92a4879d55353d46577
DaERich <daerich@sourceforge.net>
2021-06-16 22:06:58 +0200

    Fixed handler(CERT compliant);Revised Logic

--- backlightd.c
+++ backlightd.c
@@ -8,40 +8,55 @@
 
 static void handler(int);
 static void check_backlight(FILE*);
-static FILE* backlight =NULL;
-static void exit_func(char);
+static FILE* backlight = NULL;
+static void free_buffers(void);
+static volatile sig_atomic_t eflag = 0;
+static int mode = 0;
 
 int main(int argc, char**argv)
 {
 	openlog("backlight.d",LOG_PID,LOG_DAEMON);
 
-	signal(SIGINT,handler);
+	signal(SIGTERM,handler);
 	if((backlight=fopen("/sys/class/backlight/intel_backlight/brightness","r")) != NULL){
+		mode=1;
 		syslog(LOG_WARNING,"Using intel driver!");
-		check_backlight(backlight);
 	}
 	else
 		if((backlight=fopen("/sys/class/backlight/acpi_video0/brightness","r")) != NULL){
+			mode=2;
 			syslog(LOG_WARNING,"Using acpi driver!");
-			check_backlight(backlight);
 		}
 		else{
 			syslog(LOG_WARNING,"Could not find proper drivers!;Exiting!");
 			return 1;
 		}
 
-		
+	while(!eflag){
+	if( mode == 1){
+		check_backlight(backlight);
+	}
+	else
+		if( mode == 2){
+			check_backlight(backlight);
+		}
+		free_buffers();
+	}
+
+
+	printf("SIGTERM received\n");
+	if(backlight != NULL) /* Don't flush all streams */
+		fclose(backlight);
+	syslog(LOG_WARNING,"Terminating on SIGTERM");
+
+	free_buffers();
+	closelog();
 	return 0;
 }
-static void handler(int signal)
+static void handler(int signal) /* Don't use non-async logic */
 {
-	if(signal == SIGINT){
-		printf("Sigint received");
-		if(backlight != NULL) /* Don't flush all streams */
-			fclose(backlight);
-		syslog(LOG_WARNING,"Terminating on SIGINT");
-		exit_func(0);
-	}
+	if(signal == SIGTERM)
+		eflag = 1;
 }
 
 static void check_backlight(FILE* backlight)
@@ -55,15 +70,13 @@ static void check_backlight(FILE* backlight)
 	buf=addread(buf,'\0',length);
 	ptr_tbl_add(buf);
 	printf("%s",buf);
-	exit_func(0);
 }
 
-static void exit_func(char code)
+static void free_buffers(void)
 {
 	for(int x = 0;x <ptr_tbl_l;x++){
 		SAFE_FREE(ptr_tbl[x]);
 	}
-	closelog();
-	exit(code);
+	ptr_tbl_l = 0; /* Reset table(the verbose way */
 }
 
