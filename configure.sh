012ff7941ce11917f45f18a5a3190d0ed30ec485
DaERich <daerich@sourceforge.net>
2021-06-29 14:08:41 +0200

    Added option for scripted config

--- configure.sh
+++ configure.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [[ $# -eq 0 ]];then
 cat <<- "END"
 	PLEASE ENTER THE FULL PATHNAME TO THE DESIRED CONFIGURATION-FILE PATH!	
 	Example: "/home/user/config/backlightd/backlight.conf"
 	END
-read dir 
+
+read dir
+
+else
+	dir=${1}
+fi
+ 
 awk -F "=" -v CONFIGVAR=$dir '{if($1~/^CONFIG/){print ($1 "=" "\"" CONFIGVAR "\"")} else {print}}' Makefile.in | 
-	awk -F "=" -v CONFIGVAR=$dir "{if(\$1~/CONFIGMACRO/){print (\$1 \"=\" \"'\\\"\" CONFIGVAR \"'\\\"\")}
+	awk -F "=" -v CONFIGVAR=$dir "{if(\$1~/CONFIGMACRO/){print (\$1 \"=\" \"'\\\"\" CONFIGVAR \"'\\\"\")}\
 	else{print}}" > Makefile
 cat <<- "EOF" 
 	Done! 
